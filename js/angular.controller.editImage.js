d23757bc542343ac2c0af195585fec3f8268154b
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-08-09 14:39:17 +0200

    Images edit

---
+++ js/angular.controller.editImage.js
@@ -0,0 +1,33 @@
+widgeto.controller('EditImageController', function (
+        $scope, $rootScope, $http, 
+        EnableManager) {
+
+    EnableManager.add($scope);
+    $scope.isEnabled = false;
+
+    $scope.id = '';
+    $scope.value = '';
+    $scope.files = [];
+    
+    $rootScope.$on('modal-open', function (event, id, value) {
+        $scope.$apply(function () {
+            $scope.id = id;
+            $scope.value = value;
+        });
+        
+        $http.get("rest/file")
+            .then(
+                function (response) {
+                    $scope.files = response.data.files || [];
+                }
+            );
+    });
+});
+
+widgeto.run(function (TemplateManager) {
+    TemplateManager.add(
+            'edit-image', 
+            'bower_components/widget-o/html/edit-image.html', 
+            '#modal-edit');
+});
+
