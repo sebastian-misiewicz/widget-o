938c277ad0276f32b74fe639bf3dc36ff753f029
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-08-15 13:34:59 +0200

    Cleared up the edit text controller

--- js/angular.controller.editText.js
+++ js/angular.controller.editText.js
@@ -8,15 +8,6 @@ widgeto.controller('EditTextController', function (
     $scope.id = '';
     $scope.value = '';
 
-//    $rootScope.$on('modal-open', function (event, id, value) {
-//        $scope.$apply(function () {
-//            $scope.id = id;
-//            $scope.value = value;
-//        });
-//    });
-    
-    $scope.id;
-
     $scope.set = function (id) {
         console.log("Setting widget id to: " + id);
         $scope.id = id;
