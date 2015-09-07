4cf2fd65da8a5d2e63febd0f8fd35b6021529009
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-09-07 21:59:14 +0200

    Login modal - jQuery only

--- js/jquery.login.js
+++ js/jquery.login.js
@@ -1,8 +1,27 @@
-function login() {
+function showLoginModal() {
     $.get( "bower_components/widget-o/html/modal-login.html", function( data ) {
         $("body").append(data);
         $('#modal-login').modal('show');
     });
 }
 
+function login() {
+    var username = $("#modal-login-username").val(),
+        password = $("#modal-login-password").val();
 
+    $.ajax({
+        type: "POST",
+        url: "rest/login/",
+        dataType: 'json',
+        data: JSON.stringify({ 
+            "username": username,
+            "password": password
+        })
+    })
+    .done(function (data) {
+        console.log(data); 
+    })
+    .fail(function () {
+        $("#modal-login-alert").show();
+    });
+}
