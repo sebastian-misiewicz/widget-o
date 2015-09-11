cfcd37cf35956bb243e4177b5a99ef3428364905
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-09-11 22:46:44 +0200

    i18n for toolbox

--- js/angular.config.js
+++ js/angular.config.js
@@ -1,1 +1,29 @@
-widgeto = angular.module('widget-o', ['ngResource', 'ngSanitize', 'ui.sortable', 'blueimp.fileupload']);
+widgeto = angular.module('widget-o', ['ngResource', 'ngSanitize', 'ui.sortable', 'blueimp.fileupload', 'pascalprecht.translate']);
+
+widgeto.config(function ($translateProvider) {
+  $translateProvider.translations('en', {
+    CURRENT_PAGE: 'Current page',
+    SAVE: 'Save',
+    TOGGLE_WIDGETS: 'Toggle widgets',
+    REVERT_CHANGES: 'Revert changes',
+    FILES: 'Files',
+    UPLOAD: 'Upload',
+    MANAGER: 'Manager',
+    PAGES: 'Pages',
+    ADD_NEW: 'Add new',
+    LOGOUT: 'Logout'
+  });
+  $translateProvider.translations('pl', {
+    CURRENT_PAGE: 'Obecna strona',
+    SAVE: 'Zapisz',
+    TOGGLE_WIDGETS: 'Pokaż widgety',
+    REVERT_CHANGES: 'Cofnij zmiany',
+    FILES: 'Pliki',
+    UPLOAD: 'Wczytaj',
+    MANAGER: 'Manager',
+    PAGES: 'Strony',
+    ADD_NEW: 'Dodaj nową',
+    LOGOUT: 'Wyloguj'
+  });
+  $translateProvider.preferredLanguage('pl');
+});
